f037039ced3ceda5062f3ccf9b13307192205176
Robert Jackson <rjackson@Roberts-MacBook-Pro-3.local>
2024-01-18 15:04:13 -0600

    ADD: Hysplit reader version 1

--- act/io/__init__.py
+++ act/io/__init__.py
@@ -8,7 +8,7 @@ import lazy_loader as lazy
 __getattr__, __dir__, __all__ = lazy.attach(
     __name__,
 
-    submodules=['arm', 'csv', 'icartt', 'mpl', 'neon', 'noaagml', 'noaapsl', 'pysp2'],
+    submodules=['arm', 'csv', 'icartt', 'mpl', 'neon', 'hysplit', 'noaagml', 'noaapsl', 'pysp2'],
     submod_attrs={
         'arm': [
             'WriteDataset',
@@ -22,6 +22,7 @@ __getattr__, __dir__, __all__ = lazy.attach(
         'icartt': ['read_icartt'],
         'mpl': ['proc_sigma_mplv5_read', 'read_sigma_mplv5'],
         'neon': ['read_neon_csv'],
+        'hysplit': ['read_hysplit'],
         'noaagml': [
             'read_gml',
             'read_gml_co2',
